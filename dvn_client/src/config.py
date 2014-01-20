469779f363acf7a0ef0701350f5162f1ed5047b7
codersquid <github@codersquid.com>
2014-01-20 14:18:03 -0600

    allows for local settings to go in config_local

--- dvn_client/src/config.py
+++ dvn_client/src/config.py
@@ -2,3 +2,8 @@ DEFAULT_USERNAME = "FIXME"
 DEFAULT_PASSWORD = "FIXME"
 DEFAULT_HOST = "dvn-4.hmdc.harvard.edu"
 DEFAULT_CERT = "../resources/dvn-4.hmdc.harvard.edu"
+
+try:
+    from config_local import *
+except ImportError:
+    pass
