a78f7f00a2643bb5954804a2c2443538fb86cb86
icereval <icereval@gmail.com>
2015-09-29 06:20:54 -0400

    flake8 and speed up travis build

--- dataverse/settings/__init__.py
+++ dataverse/settings/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
-from dataverse.settings.defaults import *
+from dataverse.settings.defaults import *  # noqa
 
 try:
-    from dataverse.settings.local import *
+    from dataverse.settings.local import *  # noqa
 except ImportError as error:
     pass
