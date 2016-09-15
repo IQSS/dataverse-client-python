e17d8f9b8bd09b1b96cad3e61961f3833d2e486c
TomBaxter <tkb608@gmail.com>
2016-09-15 09:41:25 -0400

    Fix 'class DataverseFile' to handle old and new response format Tests were failing after swith to new server/version

--- dataverse/file.py
+++ dataverse/file.py
@@ -19,6 +19,10 @@ class DataverseFile(object):
 
     @classmethod
     def from_json(cls, dataset, json):
-        name = json['datafile']['name']
-        file_id = json['datafile']['id']
+        try:
+            name = json['dataFile']['filename']
+            file_id = json['dataFile']['id']
+        except KeyError:
+            name = json['datafile']['name']
+            file_id = json['datafile']['id']
         return cls(dataset, name, file_id)
